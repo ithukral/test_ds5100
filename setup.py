0e247878e353fcf5ecd91ba190dc712264883278
Isha Thukral <69432538+ithukral@users.noreply.github.com>
2023-07-31 16:35:09 -0400

    Create setup.py
    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='exercise9',
+
+    version='1.0.0',
+
+    url='https://github.com/ithukral/test_ds5100',
+
+    author='Isha Thukral',
+
+    author_email='van8me@virginia.edu',
+
+    description='Description of my package',
+
+    packages=find_packages(),    
+
+)
